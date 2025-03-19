75884f4df8b76bdefa350a42317fa2ab7c9c3c9a
david <tlaw4444@gmail.com>
2025-03-19 12:00:46 +0100

    Added values and CORS

--- src/main/java/com/m06/uf4/consultar/api/app/EmojisRestController.java
+++ src/main/java/com/m06/uf4/consultar/api/app/EmojisRestController.java
@@ -2,6 +2,7 @@ package com.m06.uf4.consultar.api.app;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,22 +10,49 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
+@CrossOrigin("*")
 @RequestMapping("/api/v1/emojis")
 @RestController
 public class EmojisRestController {
 
-    private final HashMap<String,String> emojisUpgrade = new HashMap<>();
+    private final HashMap<String, String> emojisUpgrade = new HashMap<>();
 
     @PostConstruct
-    public void loadEmojisOnHashMap(){
+    public void loadEmojisOnHashMap() {
+
+        //Linea evolutiva de agua
         emojisUpgrade.put("💧", "🥛");
-        emojisUpgrade.put("🥛", "Monstruo");
+        emojisUpgrade.put("🥛", "🚰");
+        emojisUpgrade.put("🚰", "🚚");
+        emojisUpgrade.put("🚚", "🏭");
+        emojisUpgrade.put("🏭", "🚂");
+        emojisUpgrade.put("🚂", "🚢");
+        emojisUpgrade.put("🚢", "🌎");
+        emojisUpgrade.put("🌎", "🚀");
+        emojisUpgrade.put("🚀", "🪐");
+        emojisUpgrade.put("🪐", "🌌");
+        emojisUpgrade.put("🌌", "🧬");
+        emojisUpgrade.put("🧬", "🤖");
+        emojisUpgrade.put("🤖", "🧠");
 
-
+        //Linea evolutiva de fuego
+        emojisUpgrade.put("🔥", "🍳");
+        emojisUpgrade.put("🍳", "🥓");
+        emojisUpgrade.put("🥓", "🍔");
+        emojisUpgrade.put("🍔", "🍕");  
+        emojisUpgrade.put("🍕", "🌭"); 
+        emojisUpgrade.put("🌭", "🍖");  
+        emojisUpgrade.put("🍖", "🍲"); 
+        emojisUpgrade.put("🍲", "🍱");  
+        emojisUpgrade.put("🍱", "🥘");  
+        emojisUpgrade.put("🥘", "🍽️"); 
+        emojisUpgrade.put("🍽️", "🎉");  
+        emojisUpgrade.put("🎉", "🍻");  
+        emojisUpgrade.put("🍻", "🎆"); 
     }
 
     @GetMapping
-    public ResponseEntity<String> getIcon(@RequestParam String emoji){
+    public ResponseEntity<String> getIcon(@RequestParam String emoji) {
         return ResponseEntity.ok(emojiUpgrade(emoji));
     }
 
